b06b2d8ea06f3b14f0dffa522358c6f53754a4e7
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:06 -0700

    feat(player_text_draw): add player_text_draw_is_visible

---
+++ pyopenmp/generated/natives/player_text_draw/player_text_draw_is_visible.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("PlayerTextDraw_IsVisible", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_void_p])
+
+
+def player_text_draw_is_visible(player, textdraw):
+    return _fn(player, textdraw)
