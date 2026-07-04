a0bd6507f0993e4604be491d2dbefa06c4227634
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:06 -0700

    feat(player_text_draw): add player_text_draw_get_color

---
+++ pyopenmp/generated/natives/player_text_draw/player_text_draw_get_color.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("PlayerTextDraw_GetColor", ctypes.c_int, [ctypes.c_void_p, ctypes.c_void_p])
+
+
+def player_text_draw_get_color(player, textdraw):
+    return _fn(player, textdraw)
