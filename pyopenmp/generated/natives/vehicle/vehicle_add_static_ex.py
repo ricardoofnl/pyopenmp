417fd987cec391048f8175d9db098738bfdc5b26
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:08 -0700

    feat(vehicle): add vehicle_add_static_ex

---
+++ pyopenmp/generated/natives/vehicle/vehicle_add_static_ex.py
@@ -0,0 +1,11 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Vehicle_AddStaticEx", ctypes.c_void_p, [ctypes.c_int, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_bool, ctypes.POINTER(ctypes.c_int)])
+
+
+def vehicle_add_static_ex(modelid, x, y, z, angle, color1, color2, respawn_delay, add_siren):
+    id = ctypes.c_int()
+    __ret = _fn(modelid, x, y, z, angle, color1, color2, respawn_delay, add_siren, ctypes.byref(id))
+    return (__ret, id.value)
