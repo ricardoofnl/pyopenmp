74d1308704aed755973381c1b5b3c912452a21f6
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:07 -0700

    feat(text_draw): add text_draw_set_color

---
+++ pyopenmp/generated/natives/text_draw/text_draw_set_color.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("TextDraw_SetColor", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_uint32])
+
+
+def text_draw_set_color(textdraw, color):
+    return _fn(textdraw, color)
