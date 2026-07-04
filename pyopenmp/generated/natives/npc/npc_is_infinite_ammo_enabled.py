d06926442244c8f57a260dd1e41a539541e72ca8
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:01 -0700

    feat(npc): add npc_is_infinite_ammo_enabled

---
+++ pyopenmp/generated/natives/npc/npc_is_infinite_ammo_enabled.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("NPC_IsInfiniteAmmoEnabled", ctypes.c_bool, [ctypes.c_void_p])
+
+
+def npc_is_infinite_ammo_enabled(npc):
+    return _fn(npc)
