8faa0a848ff8256434927178f34213180610f568
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:29:59 -0700

    feat(gang_zone): add gang_zone_is_valid

---
+++ pyopenmp/generated/natives/gang_zone/gang_zone_is_valid.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("GangZone_IsValid", ctypes.c_bool, [ctypes.c_void_p])
+
+
+def gang_zone_is_valid(gangzone):
+    return _fn(gangzone)
