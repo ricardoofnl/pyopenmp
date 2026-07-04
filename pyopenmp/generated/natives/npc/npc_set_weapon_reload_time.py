e4fe10187753e5c7ae8148a66240fe251a814774
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:01 -0700

    feat(npc): add npc_set_weapon_reload_time

---
+++ pyopenmp/generated/natives/npc/npc_set_weapon_reload_time.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("NPC_SetWeaponReloadTime", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_int, ctypes.c_int])
+
+
+def npc_set_weapon_reload_time(npc, weapon, time):
+    return _fn(npc, weapon, time)
