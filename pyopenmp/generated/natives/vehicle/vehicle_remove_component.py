c4ef31d3a1fd4fc8b66377d1e29ff7db07337b48
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:09 -0700

    feat(vehicle): add vehicle_remove_component

---
+++ pyopenmp/generated/natives/vehicle/vehicle_remove_component.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Vehicle_RemoveComponent", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_int])
+
+
+def vehicle_remove_component(vehicle, componentid):
+    return _fn(vehicle, componentid)
