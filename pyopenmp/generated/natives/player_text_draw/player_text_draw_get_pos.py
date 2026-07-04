e906601d6785234cd7733e6dda8ea0713fa1d1d7
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:06 -0700

    feat(player_text_draw): add player_text_draw_get_pos

---
+++ pyopenmp/generated/natives/player_text_draw/player_text_draw_get_pos.py
@@ -0,0 +1,14 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("PlayerTextDraw_GetPos", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)])
+
+
+def player_text_draw_get_pos(player, textdraw):
+    x = ctypes.c_float()
+    y = ctypes.c_float()
+    __ret = _fn(player, textdraw, ctypes.byref(x), ctypes.byref(y))
+    if __ret:
+        return (x.value, y.value)
+    return None
