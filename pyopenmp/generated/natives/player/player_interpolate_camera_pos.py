42a8fd16caa63916a163016fe8190f85d31c9724
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:04 -0700

    feat(player): add player_interpolate_camera_pos

---
+++ pyopenmp/generated/natives/player/player_interpolate_camera_pos.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Player_InterpolateCameraPos", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_int, ctypes.c_int])
+
+
+def player_interpolate_camera_pos(player, from_x, from_y, from_z, to_x, to_y, to_z, time, cut):
+    return _fn(player, from_x, from_y, from_z, to_x, to_y, to_z, time, cut)
