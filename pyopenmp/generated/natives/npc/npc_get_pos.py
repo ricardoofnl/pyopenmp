a9722fc347f6574c9dc6eaad2650ee3a8e27032e
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:00 -0700

    feat(npc): add npc_get_pos

---
+++ pyopenmp/generated/natives/npc/npc_get_pos.py
@@ -0,0 +1,15 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("NPC_GetPos", ctypes.c_bool, [ctypes.c_void_p, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)])
+
+
+def npc_get_pos(npc):
+    x = ctypes.c_float()
+    y = ctypes.c_float()
+    z = ctypes.c_float()
+    __ret = _fn(npc, ctypes.byref(x), ctypes.byref(y), ctypes.byref(z))
+    if __ret:
+        return (x.value, y.value, z.value)
+    return None
