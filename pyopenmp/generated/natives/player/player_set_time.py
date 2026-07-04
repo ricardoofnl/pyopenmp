1a09ac4a961496f7c9b2b41580155175693f50a7
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:05 -0700

    feat(player): add player_set_time

---
+++ pyopenmp/generated/natives/player/player_set_time.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Player_SetTime", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_int, ctypes.c_int])
+
+
+def player_set_time(player, hour, minute):
+    return _fn(player, hour, minute)
