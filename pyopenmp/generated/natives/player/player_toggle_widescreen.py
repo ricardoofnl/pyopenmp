92dc6f6366b38045ea5cb651b0ec928a74233059
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:06 -0700

    feat(player): add player_toggle_widescreen

---
+++ pyopenmp/generated/natives/player/player_toggle_widescreen.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Player_ToggleWidescreen", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_bool])
+
+
+def player_toggle_widescreen(player, enable):
+    return _fn(player, enable)
