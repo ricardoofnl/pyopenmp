e7bce0d839b8c19b2c4223353d8d511b79894eac
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:09 -0700

    feat(vehicle): add vehicle_get_trailer

---
+++ pyopenmp/generated/natives/vehicle/vehicle_get_trailer.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Vehicle_GetTrailer", ctypes.c_void_p, [ctypes.c_void_p])
+
+
+def vehicle_get_trailer(vehicle):
+    return _fn(vehicle)
