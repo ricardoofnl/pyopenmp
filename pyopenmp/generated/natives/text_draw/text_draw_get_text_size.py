61451038cea12ea15d0e337c26cfe8228b8de564
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:07 -0700

    feat(text_draw): add text_draw_get_text_size

---
+++ pyopenmp/generated/natives/text_draw/text_draw_get_text_size.py
@@ -0,0 +1,14 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("TextDraw_GetTextSize", ctypes.c_bool, [ctypes.c_void_p, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)])
+
+
+def text_draw_get_text_size(textdraw):
+    size_x = ctypes.c_float()
+    size_y = ctypes.c_float()
+    __ret = _fn(textdraw, ctypes.byref(size_x), ctypes.byref(size_y))
+    if __ret:
+        return (size_x.value, size_y.value)
+    return None
