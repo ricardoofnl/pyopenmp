1c456241c91085d3388eab42867811321bdd59aa
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:03 -0700

    feat(player_object): add player_object_is_moving

---
+++ pyopenmp/generated/natives/player_object/player_object_is_moving.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("PlayerObject_IsMoving", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_void_p])
+
+
+def player_object_is_moving(player, object):
+    return _fn(player, object)
