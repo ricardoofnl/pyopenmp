619987c1ab9882939e4613bfd19ff59f8bea3bd0
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:07 -0700

    feat(player_text_label): add player_text_label_get_pos

---
+++ pyopenmp/generated/natives/player_text_label/player_text_label_get_pos.py
@@ -0,0 +1,15 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("PlayerTextLabel_GetPos", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)])
+
+
+def player_text_label_get_pos(player, textlabel):
+    x = ctypes.c_float()
+    y = ctypes.c_float()
+    z = ctypes.c_float()
+    __ret = _fn(player, textlabel, ctypes.byref(x), ctypes.byref(y), ctypes.byref(z))
+    if __ret:
+        return (x.value, y.value, z.value)
+    return None
