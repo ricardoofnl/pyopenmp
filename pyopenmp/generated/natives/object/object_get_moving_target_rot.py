1a66f294d1d4beb7189730bc0e8d9e260fd8e42b
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:02 -0700

    feat(object): add object_get_moving_target_rot

---
+++ pyopenmp/generated/natives/object/object_get_moving_target_rot.py
@@ -0,0 +1,15 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Object_GetMovingTargetRot", ctypes.c_bool, [ctypes.c_void_p, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float)])
+
+
+def object_get_moving_target_rot(object):
+    rotation_x = ctypes.c_float()
+    rotation_y = ctypes.c_float()
+    rotation_z = ctypes.c_float()
+    __ret = _fn(object, ctypes.byref(rotation_x), ctypes.byref(rotation_y), ctypes.byref(rotation_z))
+    if __ret:
+        return (rotation_x.value, rotation_y.value, rotation_z.value)
+    return None
