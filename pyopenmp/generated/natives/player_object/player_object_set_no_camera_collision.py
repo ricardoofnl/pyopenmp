e6aac6ff377dde32ef52d48d2fcc8f74624df171
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:03 -0700

    feat(player_object): add player_object_set_no_camera_collision

---
+++ pyopenmp/generated/natives/player_object/player_object_set_no_camera_collision.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("PlayerObject_SetNoCameraCollision", ctypes.c_bool, [ctypes.c_void_p, ctypes.c_void_p])
+
+
+def player_object_set_no_camera_collision(player, object):
+    return _fn(player, object)
