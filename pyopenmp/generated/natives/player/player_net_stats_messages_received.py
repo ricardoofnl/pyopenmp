151b2f6c88cc56adcd89341afa79692366c088b3
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-03 17:30:05 -0700

    feat(player): add player_net_stats_messages_received

---
+++ pyopenmp/generated/natives/player/player_net_stats_messages_received.py
@@ -0,0 +1,9 @@
+import ctypes
+
+from pyopenmp import _capi
+
+_fn = _capi.lazy("Player_NetStatsMessagesReceived", ctypes.c_int, [ctypes.c_void_p])
+
+
+def player_net_stats_messages_received(player):
+    return _fn(player)
